4005c2d3e84f75689447663f9b93f084f5ffcd2e
Nijal Maharjan <maharjan@nijal.dev>
2024-03-29 12:11:59 +0545

    contact_us

---
+++ contact.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <script src="https://kit.fontawesome.com/b8f6f13c30.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="contact.css">
+</head>
+    <body>
+    <section id="navbar">
+      <nav
+        class="navbar navbar-expand-lg bg-body-tertiary bg-light"
+        data-bs-theme="light"
+      >
+        <div class="container-fluid">
+          <a class="navbar-brand logo" href="#">e-Raktakosh</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <div class="dropdown">
+                <li class="nav-item">
+                  <a
+                    class="nav-link active dropdown-toggle"
+                    aria-current="page"
+                    data-bs-toggle="dropdown"
+                    href=""
+                    >Home</a
+                  >
+                </li>
+                <ul class="dropdown-menu dropdown-menu-dark dropdown-content">
+                  <li><a class="dropdown-item" href="#">About</a></li>
+                  <li>
+                    <a class="dropdown-item" href="#">Why donate?</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Type of donation</a>
+                  </li>
+                  <li><a class="dropdown-item" href="#">Contact us</a></li>
+                </ul>
+              </div>
+              <div class="dropdown">
+                <li class="nav-item">
+                  <a
+                    class="nav-link  dropdown-toggle"
+                    aria-current="page"
+                    data-bs-toggle="dropdown"
+                    href=""
+                    >Looking for blood?</a
+                  >
+                </li>
+                <ul class="dropdown-menu dropdown-menu-dark dropdown-content">
+                  <li><a class="dropdown-item" href="#">Register as Patient</a></li>
+                  <li>
+                    <a class="dropdown-item" href="#">Login</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="dropdown">
+                <li class="nav-item">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    aria-current="page"
+                    data-bs-toggle="dropdown"
+                    href=""
+                    >Want to donate blood?</a
+                  >
+                </li>
+                <ul class="dropdown-menu dropdown-menu-dark dropdown-content">
+                  <li><a class="dropdown-item" href="#">Register as Donar</a></li>
+                  <li>
+                    <a class="dropdown-item" href="#">Login</a>
+                  </li>
+              </div>
+              <div class="dropdown">
+                <li class="nav-item">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    aria-current="page"
+                    data-bs-toggle="dropdown"
+                    href=""
+                    >Admin</a
+                  >
+                </li>
+                <ul class="dropdown-menu dropdown-menu-dark dropdown-content">
+                    <a class="dropdown-item" href="#">Login</a>
+                  </li>
+              </div>
+            </ul>
+            <button class="btn btn-outline-danger me-2">Login</button>
+            <button class="btn btn-danger">Sign-in</button>
+          </div>
+        </div>
+      </nav>
+    </section>
+    
+    <!-- Main section -->
+
+        <section class="body5">
+         <div class="contact">
+              <h4>Contact Us</h4>
+             <p>asdabdkjdahdajksdhksajdhasjkdhsjkadhsakjdhsadhsakdhsakdha
+        adlksadlkadasjdsajkdhasjkhsakjhajkhaskjhaskhakjsdhsa.
+             </p>
+            </div>
+            <div class="main">
+                <div class="info">
+                    <div class="box">
+                        <div class="icon"><i class="fa-solid fa-location-dot"></i></div>
+                        <div class="text">
+                            <h3>Address</h3>
+                            <p>new road ,<br>kathmandu,<br>Nepal.</p>
+                     </div>
+                    </div>
+                    <div class="box">
+                        <div class="icon"><i class="fa-solid fa-phone"></i></div>
+                        <div class="text">
+                            <h3>Phone</h3>
+                            <p>9876543210</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="icon"><i class="fa-solid fa-envelope"></i></div>
+                        <div class="text">
+                            <h3>Email</h3>
+                            <p>bloodbank@gmail.com</p>
+                        </div>
+                    </div>
+                </div> 
+                <div class="form">
+                    <form>
+                        <h2>Send Message</h2>
+                        <div class="inputBox">
+                            <input type="text" name="" required="required">
+                            <span>Full Name</span>
+                        </div>
+                        <div class="inputBox">
+                            <input type="text" name="" required="required">
+                            <span>Email</span>
+                        </div>
+                        <div class="inputBox">
+                            <textarea required="required"></textarea>
+                            <span>Type your Message...</span>
+                        </div>
+                        <div class="inputBox">
+                            <input type="submit" name="" value="Send">
+                    </div>
+                    </form>    
+                </div>
+            </div>
+        </section>
+    
+    </body>
+</html>
